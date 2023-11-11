5d583e284e1d0b90664f40375dbba4259b0519c8
Loran Maes <loran.maes@student.odisee.be>
2023-11-11 21:09:49 +0100

    mobile fixes scss

--- src/app/ui/molecules/header.js
+++ src/app/ui/molecules/header.js
@@ -1,12 +1,25 @@
+
 import React, { Component } from 'react';
 import styles from '../../page.module.scss'
 import Image from 'next/image'
 import Link from 'next/link';
 
 function Header() {
-    return ( 
+    const [expanded, setExpanded] = React.useState(false);
+
+    const changeAria = () => {
+        setExpanded(!expanded);
+    }
+
+    return (
         <header className={`${styles.header}`}>
-            <Image src={require('../../assets/logo.jpeg')} width={64} style={{borderRadius: 999}}></Image>
+            <Image className={styles.header_image} src={require('../../assets/logo.jpeg')} style={{borderRadius: 999}}></Image>
+            
+            <button onClick={changeAria} aria-expanded={expanded} className={styles.hamburger}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
         
             <nav className={`${styles.nav}`}>
                 <ul className={`${styles.nav_ul}`}>
